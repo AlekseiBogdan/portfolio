4999a5407e9887770e7449571411da48afbc1953
AlekseiBogdan <apbkgdan@gmail.com>
2023-10-27 17:46:03 +0300

    index.html and styles changes

--- static/js/mainPage.js
+++ static/js/mainPage.js
@@ -1,9 +1,40 @@
 'use strict'
 
-function check() {
-    console.log(1);
+let navScroll = (line) => {
+    document.addEventListener('scroll', () => styleChange(line));
+
+    function styleChange(line) {
+        if (window.scrollY > 120) {
+            line.style.opacity = 1;
+        } else {
+            line.style.opacity = 0;
+        }
+    }
+}
+
+let changeMode = (check) => {
+    check.addEventListener('change', () => changeStyle())
+
+    let navbar = document.getElementsByTagName('nav')
+
+    function changeStyle() {
+        if (check.checked === true) {
+            document.body.style.background = 'var(--darkmode-bg-color)';
+            navbar[0].style.background = 'var(--darkmode-bg-color)';
+        } else {
+            document.body.style.background = '#fff';
+            navbar[0].style.background = 'var(--main-bg-color)';
+        }
+    }
+
+
 }
 
 window.onload = function() {
-    check();
+    navScroll(
+        document.getElementById('navScrollLine'),
+    );
+    changeMode(
+        document.getElementById('switch'),
+    )
 }
